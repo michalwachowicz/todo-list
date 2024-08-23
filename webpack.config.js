d626fe80578638536ab7037fac4ea7d01ab215a7
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-23 21:08:25 +0200

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = (_, argv) => {
+  // Set default mode to development
+  const mode = argv.mode || "development";
+  const isProd = mode == "production";
+
+  return {
+    entry: "./src/js/index.js",
+    devtool: isProd ? "source-map" : "inline-source-map",
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "TodoApp",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "resolve-url-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+          generator: { filename: "fonts/[name][ext]" },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+          generator: { filename: "images/[name][ext]" },
+        },
+      ],
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    devServer: {
+      static: "./dist",
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new CssMinimizerPlugin(),
+        new TerserPlugin({ test: /\.js(\?.*)?$/i }),
+        new ImageMinimizerPlugin({
+          deleteOriginalAssets: true,
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: ["svgo"],
+            },
+          },
+        }),
+      ],
+    },
+  };
+};
