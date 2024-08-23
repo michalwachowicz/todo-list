933a4186a818fcb9bfe9d4badbb3c1dda44e5c04
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-24 00:32:39 +0200

    Add theme switching funcitionality

--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -1,11 +1,13 @@
+import theme from "../theme";
 import createIconButton from "./iconButton";
 
-import lightIcon from "!!raw-loader!../../assets/icons/light.svg";
-import darkIcon from "!!raw-loader!../../assets/icons/dark.svg";
 import sidebarIcon from "!!raw-loader!../../assets/icons/sidebar.svg";
 
 export default function createSidebar() {
-  const themeBtn = createIconButton(lightIcon);
+  const themeBtn = createIconButton(theme.getCurrentThemeIcon(), (e) => {
+    theme.switchTheme();
+    theme.setThemeDOM(e.target);
+  });
   const sidebarBtn = createIconButton(sidebarIcon);
 
   const sidebarHeader = document.querySelector(".sidebar-header");
---
+++ src/js/theme.js
@@ -0,0 +1,21 @@
+import lightIcon from "!!raw-loader!../assets/icons/light.svg";
+import darkIcon from "!!raw-loader!../assets/icons/dark.svg";
+
+let currentTheme = "light";
+const themeIcons = {
+  light: lightIcon,
+  dark: darkIcon,
+};
+
+const getCurrentThemeIcon = () => themeIcons[currentTheme];
+
+const setThemeDOM = (themeBtn) => {
+  themeBtn.innerHTML = getCurrentThemeIcon();
+  document.body.classList = `theme-${currentTheme}`;
+};
+
+const switchTheme = () => {
+  currentTheme = currentTheme == "light" ? "dark" : "light";
+};
+
+export default { getCurrentThemeIcon, switchTheme, setThemeDOM };
