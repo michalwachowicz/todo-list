64ffe03acb75515a1e868df5f215f4db50d2bcbb
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-25 17:27:20 +0200

    Create projects storage

--- src/js/store/projects.js
+++ src/js/store/projects.js
@@ -1,5 +1,10 @@
+import Store from "./store";
+
 const createProject = (name, color) => {
   return { name, color, dataName: name.replace(" ", "_").toLowerCase() };
 };
 
-const projects = localStorage.projects ? JSON.parse(localStorage.projects) : [];
+const projects = new Store("projects");
+const getProjects = () => projects;
+
+export default { createProject, getProjects };
