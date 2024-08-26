665b99dd65500456032409b55b77f89ffa440946
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-26 19:54:44 +0200

    Create custom select popup functionality

---
+++ src/js/components/select.js
@@ -0,0 +1,53 @@
+import Popup from "./popup";
+
+export default class Select extends Popup {
+  #list;
+
+  constructor(button, popup, input, list, onClick) {
+    super(button, popup, input, (e) => this.#onClick(e, onClick));
+    this.updateList(list);
+  }
+
+  #fill() {
+    this.getPopup().innerHTML = "";
+
+    this.#list.forEach((item) => {
+      const li = document.createElement("li");
+      li.classList = "form-select-list-item";
+
+      const btn = document.createElement("button");
+      btn.type = "button";
+      btn.classList = `btn btn-select ${item.classList}`;
+      btn.dataset.value = item.name;
+      btn.dataset.id = item.id;
+
+      if (item.icon) {
+        btn.innerHTML = item.icon + item.name;
+      } else if (item.color) {
+        const color = document.createElement("div");
+
+        color.classList = "project-color project-color-s";
+        color.style.backgroundColor = item.color;
+
+        btn.appendChild(color);
+        btn.innerHTML += item.name;
+      }
+
+      li.appendChild(btn);
+      this.getPopup().appendChild(li);
+    });
+  }
+
+  setList(list) {
+    this.#list = list;
+    this.#fill();
+  }
+
+  #onClick(e, cb) {
+    const target = e.target;
+    if (target.classList.contains("btn-select")) {
+      cb(e);
+      this.close();
+    }
+  }
+}
