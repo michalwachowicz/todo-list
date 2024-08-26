13f30bf4983ee09249339501f9a9af2e6fc216cd
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-26 12:34:56 +0200

    Create date picker functionality

---
+++ src/js/components/datePicker.js
@@ -0,0 +1,52 @@
+import calendarIcon from "!!raw-loader!../../assets/icons/calendar.svg";
+import formatDate from "../utils/date";
+
+export default class DatePicker {
+  #label;
+  #input;
+  #date;
+
+  constructor(parent, label, input, date = null) {
+    const parentElement = document.querySelector(parent);
+
+    this.#label = parentElement.querySelector(label);
+    this.#input = parentElement.querySelector(input);
+    this.#date = date;
+
+    this.#input.classList.add("form-date-picker-input");
+
+    this.#resetLabelClass();
+    this.#updateLabel();
+
+    this.#label.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.#input.showPicker();
+    });
+
+    this.#input.addEventListener("change", (e) => {
+      this.updateDate(new Date(e.target.value));
+    });
+  }
+
+  #resetLabelClass() {
+    this.#label.classList = "btn btn-label task-date";
+  }
+
+  #updateLabel() {
+    const formattedDate = formatDate(this.#date);
+    console.log(formattedDate);
+
+    this.#label.innerHTML = calendarIcon + formattedDate.formattedDate;
+    this.#label.appendChild(this.#input);
+
+    this.#resetLabelClass();
+
+    if (this.#date)
+      this.#label.classList.add(`task-date-${formattedDate.dateClass}`);
+  }
+
+  updateDate(date) {
+    this.#date = date;
+    this.#updateLabel();
+  }
+}
