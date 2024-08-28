51d73759f3bd313289e5a74921acdfbf41e87de5
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-28 14:43:12 +0200

    Change sidebar behavior on mobile screens

--- src/js/components/button/sidebarButton.js
+++ src/js/components/button/sidebarButton.js
@@ -4,7 +4,9 @@ import createIconButton from "./iconButton";
 import sidebarIcon from "!!raw-loader!../../../assets/icons/sidebar.svg";
 
 let sidebarBtn;
+
 const sidebar = document.querySelector(".sidebar");
+const background = document.querySelector(".sidebar-bg");
 
 const createSidebarBtn = (onClick) => createIconButton(sidebarIcon, onClick);
 const setSidebarBtn = (btn) => (sidebarBtn = btn);
@@ -12,12 +14,16 @@ const setSidebarBtn = (btn) => (sidebarBtn = btn);
 const showSidebarBtn = () =>
   createSidebarBtn(() => {
     visibility.show(sidebar);
+    visibility.show(background);
+
     if (sidebarBtn) visibility.hide(sidebarBtn);
   });
 
 const hideSidebarBtn = () =>
   createSidebarBtn(() => {
     visibility.hide(sidebar);
+    visibility.hide(background);
+
     if (sidebarBtn) visibility.show(sidebarBtn);
   });
 
