abefa832d9e33eb6d3480d431698fb9504361b51
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-28 15:17:04 +0200

    Create sidebar slide navigation

--- src/js/components/button/sidebarButton.js
+++ src/js/components/button/sidebarButton.js
@@ -6,6 +6,7 @@ import sidebarIcon from "!!raw-loader!../../../assets/icons/sidebar.svg";
 let sidebarBtn;
 
 const sidebar = document.querySelector(".sidebar");
+const container = document.querySelector(".sidebar-container");
 const background = document.querySelector(".sidebar-bg");
 
 const createSidebarBtn = (onClick) => createIconButton(sidebarIcon, onClick);
@@ -13,18 +14,29 @@ const setSidebarBtn = (btn) => (sidebarBtn = btn);
 
 const showSidebarBtn = () =>
   createSidebarBtn(() => {
-    visibility.show(sidebar);
     visibility.show(background);
+    visibility.show(sidebar);
 
     if (sidebarBtn) visibility.hide(sidebarBtn);
+
+    setTimeout(() => {
+      sidebar.style.transform = "translateX(0)";
+      setTimeout(() => {
+        container.style.position = "static";
+      }, 150);
+    }, 0);
   });
 
 const hideSidebarBtn = () =>
   createSidebarBtn(() => {
-    visibility.hide(sidebar);
     visibility.hide(background);
+    container.style.position = "fixed";
+    sidebar.style.transform = "translateX(-100%)";
 
-    if (sidebarBtn) visibility.show(sidebarBtn);
+    setTimeout(() => {
+      visibility.hide(sidebar);
+      if (sidebarBtn) visibility.show(sidebarBtn);
+    }, 300);
   });
 
 export { showSidebarBtn, hideSidebarBtn, setSidebarBtn };
